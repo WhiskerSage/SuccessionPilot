566f6a4c16fc65757af1cf146b9636d27719da5a
WhiskerSage <15216859336@163.com>
2026-03-04 19:46:05 +0800

    chore: align project version to 0.5.1

--- src/auto_successor/dashboard_fastapi_server.py
+++ src/auto_successor/dashboard_fastapi_server.py
@@ -37,7 +37,7 @@ def create_fastapi_app(backend: DataBackend, web_dir: Path):
 
     app = FastAPI(
         title="SuccessionPilot Dashboard API",
-        version="0.1.0",
+        version="0.5.1",
     )
     app.add_middleware(
         CORSMiddleware,
